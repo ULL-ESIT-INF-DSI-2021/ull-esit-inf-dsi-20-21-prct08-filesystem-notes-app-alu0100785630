45d5e939bb41a28260a1984f8d0f1cc16aade70e
Alberto Dorta Darias <alu0100785630@ull.edu.es>
2021-04-25 20:01:44 +0100

    Clase nota + pruebas

---
+++ tests/note.spec.ts
@@ -0,0 +1,41 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Note} from '../src/note';
+
+
+describe('Pruebas para la clase Note', () => {
+  let nota = new Note('TEST NOTE', 
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum lorem a sapien imperdiet, non facilisis nunc congue. Praesent tincidunt mauris lacinia tincidunt lacinia', 
+  'Amarillo');
+
+  it('nota es una instancia correctamente creada de la clase Note✅', () => {
+    expect(nota).to.be.instanceOf(Note);
+  });
+
+  it('El título de la nota es: "TEST NOTE"✅', () => {
+    expect(nota.getTitle()).to.be.eql('Nota de prueba');
+  });
+
+  it('El título de la nota es editable mediante el setter✅', () => {
+    nota.setTitle('NEW TITLE');
+    expect(nota.getTitle()).to.be.eql('NEW TITLE');
+  });
+
+  it('El contenido de la nota se obtiene correctamente✅', () => {
+    expect(nota.getContent()).to.be.eql('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum lorem a sapien imperdiet, non facilisis nunc congue. Praesent tincidunt mauris lacinia tincidunt lacinia');
+  });
+
+  it('El contenido de la nota es editable mediante el setter✅', () => {
+    nota.setContent('New content');
+    expect(nota.getContent()).to.be.eql('New content');
+  });
+
+  it('El color de la nota es amarillo✅', () => {
+    expect(nota.getColor()).to.be.eql('Amarillo');
+  });
+
+  it('El color de la nota es editable mediante el setter✅', () => {
+    nota.setColor('Azul');
+    expect(nota.getColor()).to.be.eql('Azul');
+  });
+});
